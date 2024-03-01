a78717a7024ba00af5e60db26ee02eb0831d1530
Sushma <sushma.patgar.4work@gmail.com>
2024-03-01 14:08:22 +0530

    files are modified

--- axiosreact/src/main.jsx
+++ axiosreact/src/main.jsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 // import './index.css'
 // import User from './Request/User.jsx'
-// import Student from './Request/Student.jsx'
+import Student from './Request/Student.jsx'
 // import Fake from './Request/Fake.jsx'
-import HelloWorld from './Request/HelloWorld.jsx'
+// import HelloWorld from './Request/HelloWorld.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <User/> */}
-    {/* <Student/> */}
+    <Student/>
     {/* <Fake/> */}
-    <HelloWorld/>
+    {/* <HelloWorld/> */}
 
   </React.StrictMode>,
 )
